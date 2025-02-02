b7d7a9e30ef45321798ad2e33f8b6baf8aa61204
TheNofis <matbey122@gmail.com>
2025-02-02 22:35:04 +0300

    add: validate

--- src/routes/user/Router.Authorize.js
+++ src/routes/user/Router.Authorize.js
@@ -4,17 +4,27 @@ const router = new Router();
 import controller from "../../controllers/user/Controller.Authorize.js";
 import validateonMiddleware from "../../middlewares/Middleware.Validateon.js";
 
-import { cookie } from "express-validator";
+import { body, cookie } from "express-validator";
 
-//router.get(
-//  "/login",
-//  [cookie("Authorization").notEmpty().length({ min: 10 })],
-//  validateonMiddleware,
-//  controller.getToken,
-//);
+// router.get(
+//   "/login",
+//   [cookie("Authorization").notEmpty().length({ min: 10 })],
+//   validateonMiddleware,
+//   controller.getToken,
+// );
 
 router.get("/notVerified", controller.notVerified);
-router.post("/register", controller.register);
+router.post(
+  "/register",
+  [
+    body("email").isEmail().normalizeEmail(),
+    body("password").isStrongPassword(),
+    body("username").isLength({ min: 3 }),
+    body("ingamename").isLength({ min: 3 }),
+  ],
+  validateonMiddleware,
+  controller.register,
+);
 router.patch("/verify", controller.verify);
 router.patch("/emailverify", controller.emailverify);
 
