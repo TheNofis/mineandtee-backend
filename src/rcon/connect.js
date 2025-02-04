7ab26b34c2cd109e668b5e61dd2fafb512541260
TheNofis <matbey122@gmail.com>
2025-02-04 21:24:59 +0300

    add: reconnect

--- src/rcon/connect.js
+++ src/rcon/connect.js
@@ -1,17 +1,24 @@
 import { Rcon } from "rcon-client";
 import "dotenv/config";
 
-const rcon = await Rcon.connect({
-  host: process.env.RCON_HOST || "play.mineandtee.fun",
-  port: process.env.RCON_PORT || 25575,
-  password: process.env.RCON_PASSWORD,
-})
-  .then((rcon) => {
-    console.log("RCON connected");
-    return rcon;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const connectRcon = async (delay = 5000) => {
+  while (true) {
+    try {
+      const rcon = await Rcon.connect({
+        host: process.env.RCON_HOST || "play.mineandtee.fun",
+        port: process.env.RCON_PORT || 25575,
+        password: process.env.RCON_PASSWORD,
+      });
+      console.log("RCON connected");
+      return rcon;
+    } catch (err) {
+      console.error(`Failed to connect to RCON: ${err.message}`);
+      console.log(`Retrying in ${delay / 1000} seconds...`);
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
+  }
+};
+
+const rcon = await connectRcon();
 
 export default rcon;
